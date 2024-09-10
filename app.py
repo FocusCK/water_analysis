ed4923f0554ad3b072160073a0f5211840a30cda
Colm Kelleher <ckcolmkelleher@gmail.com>
2024-09-10 20:51:52 +0100

    Add credentials to config

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 # Home Page
 @app.route('/')
